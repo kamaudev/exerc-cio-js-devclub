066fd3cf00363903f03b675c598d83c24cad2b08
kamaudev <contato.kamaudev@gmail.com>
2025-08-16 21:26:41 -0300

    Resolução do exercício 4

--- exercícios-módulo1.js
+++ exercícios-módulo1.js
@@ -18,3 +18,12 @@ if (numeroAleatorio > 10) {
 }else{
     console.log("Esse número é menor que 10")
 }
+
+//Programa que verifique se um número é positivo e exiba o resultado no console.//
+const valorDoExercicio = -5
+
+if (valorDoExercicio > 0) {
+    console.log("O número " + valorDoExercicio + " é positivo.");
+} else {
+    console.log("O número " + valorDoExercicio + " não é positivo.");
+}
