23dce55e277c0d7bea24d1a328b1c249011c4ceb
kamaudev <contato.kamaudev@gmail.com>
2025-08-16 21:11:08 -0300

    Resolução exercício 2

--- exercícios-módulo1.js
+++ exercícios-módulo1.js
@@ -1,3 +1,11 @@
 // Programa que exiba "Olá, mundo!" no console.//
 const mensagem = "Olá Mundo!"
 console.log(mensagem)
+
+//Programa que calcule a média de três números e exiba o resultado no console.//
+const num1 = 10;
+const num2 = 20;
+const num3 = 50;
+
+const mediaDeValores = (num1+num2+num3)/3
+console.log(mediaDeValores)
