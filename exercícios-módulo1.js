ce48ad22340b7e3b80642b7ff7001a8bde800634
kamaudev <contato.kamaudev@gmail.com>
2025-08-16 21:32:25 -0300

    Resolução do exercício 5

--- exercícios-módulo1.js
+++ exercícios-módulo1.js
@@ -26,4 +26,12 @@ if (valorDoExercicio > 0) {
     console.log("O número " + valorDoExercicio + " é positivo.");
 } else {
     console.log("O número " + valorDoExercicio + " não é positivo.");
+
 }
+//Escreva um programa que calcule a soma de dois números e exiba o resultado utilizando uma template string.//
+const numero1 = 10
+const numero2 = 48
+
+const somaDosValores = numero1 + numero2
+const resultado = `A soma dos valores ${numero1} e ${numero2} é igual a ${somaDosValores}` 
+console.log(resultado)
