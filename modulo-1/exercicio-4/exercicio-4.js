3f39d75284a34a0b3309c2e01fce49a98aa54b32
kamaudev <contato.kamaudev@gmail.com>
2025-08-29 21:50:08 -0300

    Módulo 1 - Exercício 4 - Resolução

---
+++ modulo-1/exercicio-4/exercicio-4.js
@@ -0,0 +1,9 @@
+//Programa que verifique se um número é positivo e exiba o resultado no console.//
+const valorDoExercicio = -5
+
+if (valorDoExercicio > 0) {
+    console.log("O número " + valorDoExercicio + " é positivo.");
+} else {
+    console.log("O número " + valorDoExercicio + " não é positivo.");
+
+}
--- modulo-1/exercícios-módulo1.js
+++ modulo-1/exercícios-módulo1.js
@@ -1,12 +1,4 @@
-//Programa que verifique se um número é positivo e exiba o resultado no console.//
-const valorDoExercicio = -5
 
-if (valorDoExercicio > 0) {
-    console.log("O número " + valorDoExercicio + " é positivo.");
-} else {
-    console.log("O número " + valorDoExercicio + " não é positivo.");
-
-}
 //Escreva um programa que calcule a soma de dois números e exiba o resultado utilizando uma template string.//
 const numero1 = 10
 const numero2 = 48
