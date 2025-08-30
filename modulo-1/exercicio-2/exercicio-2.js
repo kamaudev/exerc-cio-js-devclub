4b114dbc8c78937339db8f99610439b1ac81c7df
kamaudev <contato.kamaudev@gmail.com>
2025-08-29 21:46:34 -0300

    Módulo 1 - Exercício 2 - Resolução

---
+++ modulo-1/exercicio-2/exercicio-2.js
@@ -0,0 +1,7 @@
+//Programa que calcule a média de três números e exiba o resultado no console.//
+const num1 = 10;
+const num2 = 20;
+const num3 = 50;
+
+const mediaDeValores = (num1+num2+num3)/3
+console.log(mediaDeValores)
--- modulo-1/exercícios-módulo1.js
+++ modulo-1/exercícios-módulo1.js
@@ -1,12 +1,6 @@
 
 
-//Programa que calcule a média de três números e exiba o resultado no console.//
-const num1 = 10;
-const num2 = 20;
-const num3 = 50;
 
-const mediaDeValores = (num1+num2+num3)/3
-console.log(mediaDeValores)
 
 //Programa que verifique se um número é maior que 10 e exiba o resultado no console.//
 const numeroAleatorio = 8
